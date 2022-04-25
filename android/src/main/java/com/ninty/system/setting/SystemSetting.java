0d8628c6de7c536ffba439c0dec1f60dfe5842ec
Mateusz Lesiuk <mateo.lesiuk@gmail.com>
2022-04-25 10:33:38 +0200

    fix: mocked reqruired by rn 0.65 listener functions

--- android/src/main/java/com/ninty/system/setting/SystemSetting.java
+++ android/src/main/java/com/ninty/system/setting/SystemSetting.java
@@ -597,4 +597,14 @@ public class SystemSetting extends ReactContextBaseJavaModule implements Activit
             }
         }
     }
+
+    @ReactMethod
+    public void addListener(String eventName) {
+      // Keep: Required for RN built in Event Emitter Calls.
+    }
+
+    @ReactMethod
+    public void removeListeners(int count) {
+      // Keep: Required for RN built in Event Emitter Calls.
+    }
 }
